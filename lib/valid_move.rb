025aa53356fad03a6ce27850e399d9e7a14e629d
Rachel Krupnick <rachel@rachelkrupnick.com>
2015-11-23 18:55:50 +0000

    nitrous-backup at Mon Nov 23 18:55:50 UTC 2015

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,18 @@
 # code your #valid_move? method here
-
+def valid_move?(board, position)
+  if position_taken?(board, position) == true
+    return false
+  else
+    return true
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, position)
+  position.to_i
+  if (board[position] == " ") || (board[position] == "") || (board[position] == nil)
+    return false
+  else
+    return true
+  end
+end
